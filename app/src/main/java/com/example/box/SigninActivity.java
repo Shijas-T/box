e2c1afbddf7893217160e53a8055311bd4651c7d
T Muhammed Shijas <t.mohammedshijas@gmail.com>
2023-03-11 14:34:07 +0530

    Add signinActivity

--- app/src/main/java/com/example/box/SigninActivity.java
+++ app/src/main/java/com/example/box/SigninActivity.java
@@ -2,13 +2,49 @@ package com.example.box;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SigninActivity extends AppCompatActivity {
+
+    //Declaration
+    private Button buttonRegister;
+    private EditText editTextFName, editTextLName, editTextEmail, editTextPassWord, editTextPassWordConfirmation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+
+        //Hide the action bar
+        getSupportActionBar().hide();
+
+        onMenu(); //Views in menu
+        onClick();//On clickListener
     }
+
+    //All click listeners
+    private void onClick() {
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SigninActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    //Initialization (all views are initialized in this function)
+    private void onMenu() {
+        buttonRegister = findViewById(R.id.button_register);
+        editTextFName = findViewById(R.id.editText_firstName);
+        editTextLName = findViewById(R.id.editText_lastName);
+        editTextEmail = findViewById(R.id.edit_text_user_email);
+        editTextPassWord = findViewById(R.id.edit_text_password);
+        editTextPassWordConfirmation = findViewById(R.id.edit_text_password_confirmation);
+    }
+
+
 }
