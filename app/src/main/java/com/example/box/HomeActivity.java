02c75c8936fefd59e4293e7ce98743683b21e223
T Muhammed Shijas <t.mohammedshijas@gmail.com>
2023-03-11 18:39:16 +0530

    add bottom nav bar

--- app/src/main/java/com/example/box/HomeActivity.java
+++ app/src/main/java/com/example/box/HomeActivity.java
@@ -1,14 +1,50 @@
 package com.example.box;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class HomeActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class HomeActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    //Declaration
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.menu_item_home);
+    }
+
+    //Instance of fragment
+    HomeFragment homeFragment = new HomeFragment();
+    SellFragment sellFragment = new SellFragment();
+    AccountFragment accountFragment = new AccountFragment();
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menu_item_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, homeFragment).commit();
+                return true;
+
+            case R.id.menu_item_sell:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, sellFragment).commit();
+                return true;
+
+            case R.id.menu_item_account:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, accountFragment).commit();
+                return true;
+        }
+        return false;
     }
 }
---
+++ app/src/main/java/com/example/box/SellFragment.java
@@ -0,0 +1,28 @@
+package com.example.box;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SellFragment extends Fragment {
+
+    //Declaration
+    View view;
+
+    public SellFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_sell, container, false);
+
+        return view;
+    }
+}
