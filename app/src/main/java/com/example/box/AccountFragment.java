7cdf09f1d78db0c2e5612a41aa425ce676f8e45b
T Muhammed Shijas <t.mohammedshijas@gmail.com>
2023-03-12 11:43:43 +0530

    add profileFragment

--- app/src/main/java/com/example/box/AccountFragment.java
+++ app/src/main/java/com/example/box/AccountFragment.java
@@ -2,15 +2,19 @@ package com.example.box;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class AccountFragment extends Fragment {
     //Declaration
     View view;
+    private TextView textViewEdit;
 
     public AccountFragment() {
         // Required empty public constructor
@@ -22,6 +26,35 @@ public class AccountFragment extends Fragment {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_account, container, false);
 
+        onFragment();//Views in fragment
+        onClick();//On click listener
+
         return view;
+    }
+
+    // Click listeners
+    private void onClick() {
+        textViewEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "under development", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    //Initialization (all views are initialized in this function)
+    private void onFragment() {
+        textViewEdit = view.findViewById(R.id.text_view_edit);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        ((AppCompatActivity)getActivity()).getSupportActionBar().show();
     }
 }
